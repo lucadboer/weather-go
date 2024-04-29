cff62000d5ffc9ae556b54b1a1cf9fb250773821
Luca <luca.boer@outlook.com>
2024-04-29 09:27:52 -0300

    add GET /

--- main.go
+++ main.go
@@ -13,6 +13,9 @@ import (
 func main() {
 	e := echo.New()
 
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "App is running!")
+	})
 	e.GET("/weather/:cep", WeatherHandler)
 
 	e.Logger.Fatal(e.Start(":8080"))
